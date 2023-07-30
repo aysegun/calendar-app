3d28ff1c8f5535844e7d209d0f6371a09f20d88b
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-07-30 10:28:26 +0300

    created index, create method for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,21 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def index
+    @events = Event.all
+  end
+
+  def create
+    @event = Event.new
+    if @event.save
+      redirect_to events_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_time, :end_time)
+  end
 end
